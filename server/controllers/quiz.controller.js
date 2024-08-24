c76256841be071e3a4c66035833446633ef3364e
ImKetan1610 <ketan.makode@gmail.com>
2024-08-25 03:15:01 +0530

    added createQuiz method in the quiz.controller

--- server/controllers/quiz.controller.js
+++ server/controllers/quiz.controller.js
@@ -1,7 +1,46 @@
+const QuestionModel = require("../models/Question.model");
+const QuizModel = require("../models/Quiz.model");
 
+const createQuiz = async (req, res) => {
+  try {
+    let userId = req.User._id;
+    const { quizName, typeOfQuiz, questions } = req.body;
+    let arrayOfQuestion = [];
 
-const createQuiz = async() => {}
-const getQuizById = async() => {}
-const deleteQuiz = async() => {}
+    const quiz = await QuizModel.create({
+      quizName,
+      typeOfQuiz,
+      questions: arrayOfQuestion,
+      createdBy: userId,
+    });
 
-module.exports = {createQuiz, getQuizById, deleteQuiz}
+    for (let que of questions) {
+      que.quizId = quiz.id;
+      let newQue = await QuestionModel.create(que);
+      arrayOfQuestion.push(newQue.id);
+    }
+
+    quiz.questions = [...arrayOfQuestion];
+    await quiz.save();
+
+    await User.findByIdAndUpdate(userId, { $push: { quizzes: quiz.id } });
+
+    return res.status(201).json(quiz);
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error." });
+  }
+};
+const getQuizById = async (req, res) => {
+  try {
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error." });
+  }
+};
+const deleteQuiz = async (req, res) => {
+  try {
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error." });
+  }
+};
+
+module.exports = { createQuiz, getQuizById, deleteQuiz };
--- server/models/User.model.js
+++ server/models/User.model.js
@@ -15,6 +15,10 @@ const UserSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  quizzes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Quiz'
+  }],
 },{
     timestamps: true,
     versionKey:  false
