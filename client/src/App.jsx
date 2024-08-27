04c539b3e41ab5831503206b2253801b5c301a1d
ImKetan1610 <ketan.makode@gmail.com>
2024-08-28 02:55:54 +0530

    added login and register pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,28 @@
-import { useState } from "react";
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import DashBoard from "./components/DashBoard";
+import AuthPage from "./components/Pages/AuthPages/AuthPage";
+import Login from "./components/Auth/Login/Login";
+import Register from "./components/Auth/Register/Register";
 
 function App() {
-  return <>App</>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage />,
+      children: [{ path: "dashboard", element: <DashBoard /> }],
+    },
+    {
+      path: "/auth",
+      element: <AuthPage />,
+      children: [
+        { path: "login", element: <Login /> },
+        { path: "register", element: <Register /> },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
